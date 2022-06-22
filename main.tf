1cc3b85259c094b5f6bec53c508859d17e5b344b
Ash Dey <ashabc@hotmail.com>
2022-06-22 17:04:47 +1000

    initial config-2

--- main.tf
+++ main.tf
@@ -20,9 +20,9 @@ module "network" {
     source              = "Azure/vnet/azurerm"
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
-    address_space       = var.address_space "10.0.0.0/16"
-    subnet_prefixes     = var.subnet_prefixes ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    subnet_names        = var.subnet_names ["subnet1", "subnet2", "subnet3"]
+    address_space       = var.address_space
+    subnet_prefixes     = var.subnet_prefixes
+    subnet_names        = var.subnet_names
 
     tags = "${merge( local.common_tags, local.extra_tags)}"
 }
